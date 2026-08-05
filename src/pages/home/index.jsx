1151f59870642ab89a03de119825b50721be1e2c
WesleyRNX09 <hello.wesleyberaldi@gmail.com>
2026-08-05 08:12:29 -0300

    05-08
    fazendo a Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,47 @@
-import { useState } from 'react';
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
-
+import banner from '../../assets/banner.png';
 import styles from './index.module.css';
 
 function Home() {
+  const [rolouPagina, setRolouPagina] = useState(false);
+
+  useEffect(() => {
+    function verificarScroll() {
+      setRolouPagina(window.scrollY > 50);
+    }
+
+    window.addEventListener('scroll', verificarScroll);
+
+    return () => {
+      window.removeEventListener('scroll', verificarScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.pagina}>
+      <header
+        className={`${styles.barraPrincipal} ${
+          rolouPagina ? styles.barraRolada : ''
+        }`}
+      >
+        <h1>Minha Hamburgueria</h1>
+      </header>
 
-      
+      <section
+        className={styles.banner}
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className={styles.conteudoBanner}>
+          <h1>O verdadeiro hambúrguer artesanal</h1>
+        </div>
+      </section>
 
-      <h1>Bem-vindo à Hamburgueria!</h1>
-
-      
+      <section className={styles.cardapio}>
+        <h2>Nosso cardápio</h2>
+      </section>
     </div>
-  )
+  );
 }
 
 export default Home;
